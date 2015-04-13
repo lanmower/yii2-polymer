bb54640d7a1f769bc9aabe245fd0ce881d911705
James Vos <almagestfraternite@gmail.com>
2015-04-13 14:44:59 +0200

    modified:   widgets/CoreItem.php

--- widgets/CoreItem.php
+++ widgets/CoreItem.php
@@ -12,10 +12,12 @@ class CoreItem extends PolymerWidget {
 	public $tag = 'core-item';
 	public $icon;
 	public $label = '';
+	public $url = false;
 	public function init() {
 		$this->htmlOptions ['icon'] = $this->icon;
 		$this->htmlOptions ['label'] = $this->label;
 		return parent::init ();
+		if($this->url) echo Html::a('',$this->url);
 	}
 }
 ?>
